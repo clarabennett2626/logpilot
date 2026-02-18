65d2a01ffdfd850767ad5a6ba2e0e4226341c490
Clara Bennett <clarabennett2626@gmail.com>
2026-02-17 21:08:45 -0500

    Create main.go

---
+++ cmd/logpilot/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/clarabennett2626/logpilot/internal/tui"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Printf("logpilot %s (%s) built %s\n", version, commit, date)
+		os.Exit(0)
+	}
+
+	p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
